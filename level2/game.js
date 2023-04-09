bdce11cd9ad67fd3c71b1178574a4e2b39277eaa
IdinGanteng <kanggayus101@gmail.com>
2023-04-09 23:01:33 +0700

    game

---
+++ level2/game.js
@@ -0,0 +1,38 @@
+let question = document.getElementById("question");
+let answer = document.getElementById("answer");
+let result = document.getElementById("result");
+let next = document.getElementById("next");
+let num1, num2, op, correctAnswer;
+generateQuestion();
+document.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  if (parseInt(answer.value) == correctAnswer) {
+    result.innerHTML = "Yeey Kamu Benar!";
+    next.style.display = "inline";
+  } else {
+    result.innerHTML = "Uppss, Bodoh sekali anda!";
+  }
+});
+document.addEventListener("click", function(event) {
+  if (event.target.id == "next") {
+    generateQuestion();
+    answer.value = "";
+    result.innerHTML = "";
+    next.style.display = "none";
+  }
+});
+function generateQuestion() {
+  num1 = Math.floor(Math.random() * 100) + 1;
+  num2 = Math.floor(Math.random() * 100) + 1;
+  op = Math.floor(Math.random() * 2);
+  
+  if (op == 0) {
+    question.innerHTML = num1 + " + " + num2 + " = ?";
+    correctAnswer = num1 + num2;
+  } else {
+    question.innerHTML = num1 + " - " + num2 + " = ?";
+    correctAnswer = num1 - num2;
+  }
+  
+}
